736078aac1a09a38bf68ce83f8482627f2637aa2
ahmedessam228 <ahmedeliwa228@gmail.com>
2024-10-01 21:47:01 +0300

    Commit StudentCOntroller Done

--- ITIAcademy/Controllers/StudentController.cs
+++ ITIAcademy/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITIAcademy.Data;
+using ITIAcademy.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITIAcademy.Controllers
 {
@@ -8,6 +10,63 @@ namespace ITIAcademy.Controllers
         {
             return View();
         }
+        AppDbContext context = new AppDbContext();
+
+        // /Student/GetAll
+        public IActionResult GetAll()
+        {
+            List<Student> students = context.Students.ToList();
+            return View(students);
+        }
+
+        // /Student/GetById/1
+        public IActionResult GetById(int id)
+        {
+            Student student = context.Students.FirstOrDefault(i => i.Id == id);
+            return View(student);
+        }
+
+
+        #region InsertNewStudent
+        // /Student/GoToAddForm
+        public IActionResult GoToAddForm()
+        {
+            return View();
+        }
+
+        public IActionResult SaveFormData(Student student)
+        {
+            context.Students.Add(student);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        #endregion
+
+        #region EditStudent
+        public IActionResult SaveEditFormData(Student student)
+        {
+            context.Students.Update(student);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+
+        // /Student/GoToEditForm/1
+        public IActionResult GoToEditForm(int id)
+        {
+            Student student = context.Students.FirstOrDefault(i => i.Id == id);
+            return View(student);
+        }
+        #endregion
+
+        #region DeleteStudent
+        public IActionResult Delete(int id)
+        {
+            Student student = context.Students.FirstOrDefault(e => e.Id == id);
+            context.Students.Remove(student);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        #endregion
     }
 
 
---
+++ ITIAcademy/Migrations/20241001182054_InsertTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITIAcademy.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
