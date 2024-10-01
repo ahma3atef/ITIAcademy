aca680286d67c5a5823a5dabbc6c2edcbc2af565
ahmedessam228 <ahmedeliwa228@gmail.com>
2024-10-01 14:15:37 +0300

    Commit CourseController Done

--- ITIAcademy/Controllers/CourseController.cs
+++ ITIAcademy/Controllers/CourseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITIAcademy.Data;
+using ITIAcademy.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITIAcademy.Controllers
 {
@@ -8,5 +10,64 @@ namespace ITIAcademy.Controllers
         {
             return View();
         }
+        AppDbContext context = new AppDbContext(); 
+        
+        // /Course/GetAll
+        public IActionResult GetAll()
+        {
+            List<Course> courses = context.Courses.ToList();
+            return View(courses);
+        }
+
+        // /Course/GetById/1
+        public IActionResult GetById(int id) 
+        {
+            Course course = context.Courses.FirstOrDefault(c => c.Id == id);
+            
+            return View(course);
+        }
+
+
+        #region InsertNewCourse
+        // /Course/GoToAddForm
+        public IActionResult GoToAddForm()
+        {
+            return View();
+        }
+
+        public IActionResult SaveFormData(Course course)
+        {
+            context.Courses.Add(course);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        #endregion
+
+        #region EditCourse
+        public IActionResult SaveEditFormData(Course course)
+        {
+            context.Courses.Update(course);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+
+        // /Course/GoToEditForm/1
+        public IActionResult GoToEditForm(int id)
+        {
+            Course course = context.Courses.FirstOrDefault(e => e.Id == id);
+            return View(course);
+        }
+        #endregion
+
+        #region DeleteCourse
+        public IActionResult Delete(int id)
+        {
+            Course course = context.Courses.FirstOrDefault(e => e.Id == id);
+            context.Courses.Remove(course);
+            context.SaveChanges();  
+            return RedirectToAction("GetAll");
+        }
+        #endregion
+
     }
 }
